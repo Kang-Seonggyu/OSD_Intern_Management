5b055d6f4c7922bb8c4131f867c49e1a5f6c63fa
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-14 13:07:50 +0900

    인턴 페이지 만들기

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import "./style.css"
+
+function Header() {
+  return (
+    <div className="headContainer">
+        <img
+            src="https://onesoftdigm.com/img/logo.png"
+            alt="https://onesoftdigm.com/img/logo.png"
+            className="headImg"
+        />
+    </div>
+  );
+}
+
+export default Header;
