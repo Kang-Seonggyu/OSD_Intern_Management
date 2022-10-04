fdaee6e2be2bb1f921a35531da08fdf5a8f33999
Kang-Seonggyu <jokekim609@gmail.com>
2022-10-05 08:56:04 +0900

    공용으로 쓸 버튼 컴포넌트 만들어보기

--- src/Components/Common/Button.js
+++ src/Components/Common/Button.js
@@ -1,14 +1,16 @@
 import styled, {css} from "styled-components";
+import {Link} from "react-router-dom";
 
 const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
   font-weight: bold;
-  padding : 0.25rem 1rem;
-  color : white;
-  outline : none;
-  cursor : pointer;
+  padding: 0.25rem 1rem;
+  background: white;
+  color: black;
+  outline: none;
+  cursor: pointer;
 `
 const StyledButton = styled.button`
     ${buttonStyle}
