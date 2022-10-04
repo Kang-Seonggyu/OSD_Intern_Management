fa19f51d3446d2144e0f364bb61bdab461a49652
Kang-Seonggyu <jokekim609@gmail.com>
2022-10-04 11:31:27 +0900

    메뉴 클릭 에니메이션 적용

--- src/Container/Common/HeaderContainer.js
+++ src/Container/Common/HeaderContainer.js
@@ -3,11 +3,24 @@ import "./Hearder.css"
 
 function HeaderContainer() {
     const menuClick = () => {
+        const menuToggle = document.getElementById("menu")
 
+        if (menuToggle.className === 'menu odd') {
+            menuToggle.className = 'menu even'
+        }
+        else if (menuToggle.className === 'menu even') {
+            menuToggle.className = 'menu odd'
+        }
+        else {
+            menuToggle.className = 'menu odd'
+        }
     }
+
   return (
     <div className="HeaderContainer" id="HeaderContainer">
-        <img src="https://cdn-icons-png.flaticon.com/512/2976/2976215.png" alt="Menu" style={{ height : "30px", marginLeft : "10px", cursor : "pointer"}} />
+        <span className="menu-box">
+            <img className="menu" id="menu" src="https://cdn-icons-png.flaticon.com/512/2976/2976215.png" alt="Menu" onClick={menuClick}/>
+        </span>
         <a href="https://onesoftdigm.com/index.php?getLang=ko">
             <img src="https://onesoftdigm.com/img/logo.png" alt="원소프트다임, Logo" style={{ height : "30px"}} className="headImg" />
         </a>
