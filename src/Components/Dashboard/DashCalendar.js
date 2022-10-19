4c062e1cc186459a6d5200ef2df101e1e3fd84da
Kang-Seonggyu <jokekim609@gmail.com>
2022-10-19 10:35:47 +0900

    디테일 ( 마우스 올릴 때 설명 )

--- src/Components/Dashboard/DashCalendar.js
+++ src/Components/Dashboard/DashCalendar.js
@@ -204,11 +204,11 @@ function DashCalendar({onClick}) {
     return (
         <>
             <ControllerBlock>
-                <ControlButton onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'year')) }}>«</ControlButton>
-                <ControlButton onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }}>‹</ControlButton>
+                <DControlButton title="1년전" onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'year')) }}>«</DControlButton>
+                <DControlButton title="1달전" onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }}>‹</DControlButton>
                 <span>{today.format('YY 년 MM 월')}</span>
-                <ControlButton onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }}>›</ControlButton>
-                <ControlButton onClick={()=>{ setMoment(getMoment.clone().add(1, 'year')) }}>»</ControlButton>
+                <DControlButton title="1달후" onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }}>›</DControlButton>
+                <DControlButton title="1년후" onClick={()=>{ setMoment(getMoment.clone().add(1, 'year')) }}>»</DControlButton>
             </ControllerBlock>
             <CalendarBlock>
                 <CalendarIndex>
